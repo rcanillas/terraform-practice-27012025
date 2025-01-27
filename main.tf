b3472ac4ec00ae6ee5cf6eeed1012a4b662f0591
Rémi Canillas <remi.canillas@gmail.com>
2025-01-27 14:00:16 +0100

    add repo creation

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "github_user_ssh_key" "repo_key" {
     title   = "Terraform SSH Key"
     key     = file("~/.ssh/id_ras.pub")     
 }
+
+# Step 2: Create a GitHub repository
+resource "github_repository" "terraform_repo" {
+  name        = "my-terraform-repo"
+  description = "A Terraform-managed repository"  
+  visibility  = "public"                
+
+  # Optional settings
+  has_issues         = true
+}
